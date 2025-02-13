8f1930fbfa833bd6954f8b35a8ccd5289caff51b
ohaddahan <ohaddahan@gmail.com>
2025-02-13 17:52:51 +0200

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -14,4 +14,4 @@ echo \
 sudo apt-get update
 
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
