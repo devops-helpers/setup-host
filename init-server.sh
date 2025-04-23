a0d78f22459481e84f1bc3e7b365afd76bf340a8
ohaddahan <ohaddahan@gmail.com>
2025-04-23 22:47:33 +0300

    Update init-server.sh

--- init-server.sh
+++ init-server.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y python3 python3-pip git
 sudo apt install build-essential -y
 sudo apt-get install libssl-dev -y
 sudo apt install pkg-config -y
+sudo apt-get install gdebi-core -y
 sudo apt-get install -y cmake clang
