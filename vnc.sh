5fe132596ef3a6231da645ee7dff47130acd8a8f
ohaddahan <ohaddahan@gmail.com>
2025-04-23 19:03:32 +0300

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -19,3 +19,13 @@ chmod +x ~/.vnc/xstartup
 
 vncserver :1
 
+# Make it look like MacOs
+# https://medium.com/featurepreneur/make-ubuntu-look-like-macos-fd11c6ccf83a
+sudo apt install gnome-tweaks -y
+sudo apt install gtk2-engines-murrine gtk2-engines-pixbuf -y
+sudo apt install sassc optipng inkscape libglib2.0-dev-bin -y
+
+
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
+cd WhiteSur-icon-theme/
+./install.sh
